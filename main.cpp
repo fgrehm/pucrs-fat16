8bc850ab562e8729d2883c7ccea8c7e22a463e20
Fabio Rehm <fgrehm@gmail.com>
2015-05-27 22:10:06 -0300

    Parse the command to be executed

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 #include <string>
 
+std::string parse_command(const std::string& input) {
+  return input.substr(0, input.find(' '));
+}
+
 int main() {
   std::cout << "PUCRS FAT16 SHELL\n";
   std::cout << "$ ";
 
-  std::string name;
-  getline (std::cin, name);
-  std::cout << "-> Will execute: " << name << "\n";
+  std::string cmd;
+  getline (std::cin, cmd);
+  std::cout << "-> Will execute: " << parse_command(cmd) << "\n";
 }
