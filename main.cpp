70e739b30d8d33aeaae53eba46e9a3ab302d7c9e
Mateus Vendramini <mateusvendramini21@gmail.com>
2015-06-01 21:49:55 -0300

    adding exit command

--- main.cpp
+++ main.cpp
@@ -1,16 +1,26 @@
 #include <iostream>
+#include <stdlib.h>
 #include <string>
+
+void run_command(const std::string &cmd){
+
+  if (cmd == "exit"){
+    exit(0);
+  } 
+
+}
 
 std::string parse_command(const std::string& input) {
   return input.substr(0, input.find(' '));
 }
 
 int main() {
-  std::cout << "PUCRS FAT16 SHELL\n";
+  std::cout << "PUCRS FAT16 SHELL" << std::endl;
   while (true) {
     std::cout << "$ ";
     std::string cmd;
     getline (std::cin, cmd);
-    std::cout << "-> Will execute: " << parse_command(cmd) << "\n";
+    run_command(parse_command(cmd));
   }
+  return 0;
 }
