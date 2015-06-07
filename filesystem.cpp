65e9cc5d8caa1b7425f47648449f980e914efaf7
Mateus Vendramini <mateusvendramini21@gmail.com>
2015-06-07 20:07:15 -0300

    full interface specification prototype

--- filesystem.cpp
+++ filesystem.cpp
@@ -7,3 +7,30 @@ FileSystem::FileSystem(){
 FileSystem::~FileSystem(){
 }
 
+int FileSystem::init(){
+}
+
+int FileSystem::load(){
+}
+
+int FileSystem::listdir(const std::string &path, std::vector<std::string> &result){
+}
+
+int FileSystem::makedir(const std::string &path){
+}
+
+int FileSystem::createfile(const std::string &path){
+}
+
+int FileSystem::unlink(const std::string &path){
+}
+
+int FileSystem::write(const std::string &path, const std::string &content){
+}
+
+int FileSystem::append(const std::string &path, const std::string &content){
+}
+
+int FileSystem::read(const std::string &path, std::string &content){
+}
+
--- filesystem.h
+++ filesystem.h
@@ -2,6 +2,21 @@
 #ifndef _FILESYSTEM_H_
 #define _FILESYSTEM_H_
 
+#include "fat16.h"
+#include "rootdir.h"
+#include "datablocks.h"
+
+#include <vector>
+#include <string>
+
+#define RET_OK 0
+#define RET_NONEXISTANT_PATH 1
+#define RET_NONEXISTANT_FILE 2
+#define RET_DIR_ALREADY_EXISTS 3
+#define RET_FILE_ALREADY_EXISTS 4
+#define RET_FS_FULL 5
+#define RET_DIR_NOT_EMPTY 6
+
 class FileSystem {
 
   public:
@@ -10,7 +25,17 @@ class FileSystem {
     ~FileSystem();
 
     /* Filesystem interface begin */
-    void format();
+    int init(); // resets the virtual FS and writes it to the real disk
+    int load(); // loads the virtual FS from the real disk
+
+    int listdir(const std::string &, std::vector<std::string> &); // reads the contents of a path
+    int makedir(const std::string &); // makes a new dir on the given path
+    int createfile(const std::string &); // creates a file on the given path
+    int unlink(const std::string &); // deletes file or dir on the given path
+
+    int write(const std::string &, const std::string &); // writes string to the given file
+    int append(const std::string &, const std::string &); // appends string to the given file
+    int read(const std::string &, std::string &); // reads the given file onto the given string
     /* Filesystem interface end */
 
   private:
