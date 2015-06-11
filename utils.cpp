56f7630ada6e2437f767f7fc222a51a60eef46cb
Fabio Rehm <fgrehm@gmail.com>
2015-06-11 00:28:43 -0300

    Implement split and join functions

--- utils.cpp
+++ utils.cpp
@@ -1,3 +1,7 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
 
 #include "utils.h"
 
@@ -26,3 +30,31 @@ std::vector<std::string> tokenize_path(const std::string &path){
   return ret;
 }
 
+// http://stackoverflow.com/a/236803/2738223
+std::vector<std::string> &split(const std::string &s, char delim, std::vector<std::string> &elems) {
+  std::stringstream ss(s);
+  std::string item;
+  while (std::getline(ss, item, delim)) {
+    if (item != "")
+      elems.push_back(item);
+  }
+  return elems;
+}
+std::vector<std::string> split(const std::string &s, char delim) {
+  std::vector<std::string> elems;
+  split(s, delim, elems);
+  return elems;
+}
+
+std::string join(const std::vector<std::string> &vec, std::string delim) {
+  std::stringstream ss;
+  std::vector<std::string>::const_iterator iter = vec.begin();
+  while(iter != vec.end()) {
+    ss << *iter;
+    ++iter;
+
+    if (iter != vec.end())
+      ss << delim;
+  }
+  return ss.str();
+}
