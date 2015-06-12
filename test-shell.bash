60790143eac3263a8f6e8f721f7f02fe6734263d
Fabio Rehm <fgrehm@gmail.com>
2015-06-12 19:07:16 -0300

    Ensure the shell picks up wrong commands as well

--- test-shell.bash
+++ test-shell.bash
@@ -1,22 +1,23 @@
 #!/usr/bin/env basht
 
-# Requires https://github.com/progrium/basht to run these tests
+# Use https://github.com/progrium/basht to run these tests
 
-###########################################################
-# Simple helper to run the shell passing in some argument
-###########################################################
 run() {
   echo $1 | ./pucrs-fat16 2>&1
 }
 
-###########################################################
-# Tests
-###########################################################
 T_recognizes_all_valid_commands() {
   for cmd in init load ls mkdir create unlink write append read; do
     if run "${cmd}" | grep -q ': command not found$'; then
-      $T_fail "'${cmd}' was not recognized as a valid parameter"
+      $T_fail "'${cmd}' was not recognized as a valid command"
       return 1
     fi
   done
+}
+
+T_recognizes_invalid_commands() {
+  if ! run "WAT" | grep -q ': command not found$'; then
+    $T_fail "'WAT' was recognized as a valid command"
+    return 1
+  fi
 }
